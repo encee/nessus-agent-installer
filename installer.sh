1932e0655ba45e446098184608b290be1b20538d
Noah Commenator <commenator.noah@gmail.com>
2017-06-06 14:23:55 -0600

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+getHostInfo(){
+    echo "Gathering host info..."
+    if $(hash lsb_release &> /dev/null); then
+        OS=$(lsb_release -si|tr '[:upper:]' '[:lower:]')
+        OS_VER=$(lsb_release -sr|tr -d '.')
+        OS_CNAME=$(lsb_release -sc|tr '[:upper:]' '[:lower:]')
+        OS_DESC=$(lsb_release -sd|tr '[:upper:]' '[:lower:]')
+    elif [[ -f /etc/os-release ]]; then
+        soure /etc/os-release
+        OS=$(echo $DISTRIB_ID |tr '[:upper:]' '[:lower:]')
+        OS_VER=$(echo $DISTRIB_RELEASE |tr -d '.')
+        OS_CNAME=$(echo $DISTRIB_CODENAME |tr '[:upper:]' '[:lower:]')
+        OS_DESC=$(echo $DISTRIB_DESCRIPTION |tr '[:upper:]' '[:lower:]')
+    elif [[ -f /etc/redhat-release ]]; then
+        OS=$(cat /etc/centos-release |awk {'print $1'} |tr '[:upper:'] '[:lower:]')
+        OS_VER=$(cat /etc/centos-release |awk {'print $3'}|awk -F"." {'print $1'})
+        OS_CNAME="$OS $OS_VER"
+        OS_DESC="$OS_CNAME"
+    else
+        echo "Unable to determine host information"
+        exit 1
+    fi
+    ARCH=$(uname -m)
+    echo "Host is $OS - $OS_VER"
+}
+
+downloadClient(){
+    filePrefix="NessusAgent-6.10.7"
+    fileDelim="."
+    if [ ! -z $OS ] && [ ! -z $OS_VER ]; then
+        case $OS in
+            "ubuntu" )
+                case $OS_VER in
+                    1110 | 1204 | 1210 | 1304 | 1310 | 1404 | 1604 |1704)
+                        OS_FILE="ubuntu1110"
+                    ;;
+                    910 | 1004 )
+                        OS_FILE="ubuntu910"
+                    ;;
+                    * )
+                        echo "Unsupported version $OS - $OS_VER"
+                        exit 1
+                esac
+                fileExt="deb"
+                fileDelim="_"
+                ARCH=$(echo $ARCH |sed -e 's/x86_64/amd64/')
+            ;;
+            "debian" )
+                case $OS_VER in
+                    6 | 7 | 8 )
+                        OS_FILE="debian6"
+                        fileExt="deb"
+                    ;;
+                    * )
+                    echo "Unsupported version $OS - $OS_VER"
+                    exit 1
+                esac
+                fileDelim="_"
+                fileExt="deb"
+            ;;
+            "fedora" )
+                case $OS_VER in
+                    20 | 21 )
+                        OS_FILE="fc20"
+                    ;;
+                    * )
+                        echo "Unsupported version $OS - $OS_VER"
+                        exit 1
+                esac
+                fileExt="rpm"
+                ARCH=$(echo $ARCH |sed -e 's/x86_64/amd64/')
+            ;;
+            "centos" | "redhat" )
+                case $OS_VER in
+                    5 | 6 | 7 )
+                        OS_FILE="es${OS_VER}"
+                    ;;
+                    * )
+                        echo "Unsupported version $OS - $OS_VER"
+                        exit 1
+                    ;;
+                esac
+                fileExt="rpm"
+            ;;
+            "amzn" )
+            ;;
+        esac
+    else
+        echo "Undetected os and os version"
+        exit 1
+    fi
+    fileSuffix="${OS_FILE}${fileDelim}${ARCH}.${fileExt}"
+
+    echo "Acquiring tenable timestamp..."
+    con="https://downloads.nessus.org/nessus3dl.php?file=${filePrefix}-${fileSuffix}&licence_accept=yes"
+    timestamp=$(wget -qO- $con|grep 'timecheck' |awk -F">" {'print $5'}|awk -F"<" {'print $1'})
+
+    echo "Downloading file - $fileSuffix"
+    fullCon="${con}&t=${timestamp}"
+    wget -O $fileSuffix $fullCon
+}
+
+installClient(){
+    echo "Installing file - $fileSuffix"
+    case $fileExt in
+        "rpm" )
+            if $(hash yum &> /dev/null); then
+                install_command="sudo yum install -y"
+            else
+                echo "Unable to locate yum installer for $fileSuffix."
+                exit 1
+            fi
+        ;;
+        "deb")
+            if $(hash dpkg &> /dev/null); then
+                install_command="DEBIAN_FRONTEND=noninteractive sudo dpkg -i"
+            else
+                echo "Unable to locate dpkg installer for $fileSuffix"
+            fi
+        ;;
+        * )
+            echo "Unknown package type $fileExt"
+            exit 1
+        ;;
+    esac
+    eval "$install_command $fileSuffix"
+}
+
+main(){
+  getHostInfo
+  downloadClient
+  installClient
+}
+
+main
